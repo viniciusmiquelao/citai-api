d207345a9ff7e562a0d191cdc70083ff0448906e
Vinícius <viniciuscarvalho60@yahoo.com.br>
2022-03-30 00:12:43 -0300

    Confi dotenv

--- api/asgi.py
+++ api/asgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
+from dotenv import load_dotenv
 
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+
+load_dotenv()
 
 application = get_asgi_application()
--- api/wsgi.py
+++ api/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
+from dotenv import load_dotenv
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+
+load_dotenv()
 
 application = get_wsgi_application()
